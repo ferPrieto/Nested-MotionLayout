5c0ea948e7f58c28de2b9d89f0eef8b7847ae986
fernando <fernando.moyano@nice.agency>
2020-01-16 16:50:13 +0000

    Added project skelleton and build gradle kts

---
+++ buildSrc/src/main/java/prieto/fernando/buildsrc/Dependencies.kt
@@ -0,0 +1,44 @@
+object AndroidSettings {
+    const val appVersionName = "0.1"
+    const val compileSdk = 29
+    const val buildTools = "29.0.2"
+    const val minSdk = 26
+    const val targetSdk = 29
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
+
+object Versions {
+    const val appCompat = "1.1.0"
+    const val kotlin = "1.3.60"
+    const val gradle = "3.5.1"
+    const val constraintLayout = "2.0.0-beta3"
+    const val lifeCycle = "2.0.0"
+    const val material = "1.1.0-alpha05"
+    const val lifecycleLivedataKtx = "2.2.0"
+    const val glide = "4.8.0"
+}
+
+object BuildDependencies {
+    const val androidGradle =
+            "com.android.tools.build:gradle:${Versions.gradle}"
+    const val kotlinGradlePlugin =
+            "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+}
+
+object Dependencies {
+
+    object AndroidX {
+        const val fragmentKtx =
+                "androidx.fragment:fragment-ktx:${Versions.appCompat}"
+        const val coreKtx =
+                "androidx.core:core-ktx:${Versions.appCompat}"
+        const val constraintlayout =
+                "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+        const val archComponents =
+                "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycleLivedataKtx}"
+
+    }
+
+    const val material= "com.google.android.material:material:${Versions.material}"
+    const val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "FancyMotionLayout"
+rootProject.buildFileName = "build.gradle.kts"
+
+include(
+    ":app"
+)
